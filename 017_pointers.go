e15361c750943e4c02c2ba12508f7c11d8df9b4d
Suboch Kirill <k.suboch@1c-tech.info>
2017-10-16 12:18:01 +0400

    Pointers

---
+++ 017_pointers.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	i := 1
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i) // &i syntax gives memory address of i
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+}
